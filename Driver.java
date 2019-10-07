208ce54bc8267bc6a6d5abef94eaabad7793f718
beanjamean <benvan11@icloud.com>
2019-10-07 12:16:16 -0500

    Nwe File

---
+++ Driver.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class Driver 
+{
+	public static void main(String[] args)
+	{
+		String s = "Ben";
+		char c = 'H';
+		
+		System.out.println(s.toUpperCase());
+		System.out.println(Character.toUpperCase(c));
+	}
+		
+}
